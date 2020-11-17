8a8f8a3f98517db87988a499ee2018ae7b5b218d
Anh Nguyen <anhnguyendon92@gmail.com>
2020-11-17 15:13:18 -0800

    touch up

---
+++ models/burger.js
@@ -0,0 +1,30 @@
+const orm = require('../config/orm.js');
+
+const burger = {
+  selectAll: (cb) => {
+    orm.selectAll('burgers', (res) => {
+      cb(res);
+    });
+  },
+
+  insertOne: (cols, vals, cb) => {
+    orm.insertOne('burgers', cols, vals, (res) => {
+      cb(res);
+    });
+  },
+
+  updateOne: (objColVals, condition, cb) => {
+    orm.updateOne('burgers', objColVals, condition, (res) => {
+      cb(res);
+    });
+  },
+
+  deleteOne: (condition, cb) => {
+    orm.deleteOne('burgers', condition, (res) => {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller ().
+module.exports = burger;
