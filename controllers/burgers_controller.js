708a4aa861e82ddd8b8fe39a543d58e098674462
Anh Nguyen <anhnguyendon92@gmail.com>
2020-11-17 05:12:45 -0800

    controllers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,57 @@
+const burger = require('../models/burger');
+
+const express = require('express');
+
+const router = express.Router();
+
+// Create all routes and setting up logic
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+      const hdbrsObj = {
+        burgers: data
+      };
+      console.log(hdbrsObj);
+      res.render('index', hdbrsObj);
+    });
+  
+    router.post('/api/burgers', (req, res) => {
+      burger.insertOne(
+        ['burger_name', 'devoured'],
+        [req.body.burger_name, req.body.devoured],
+        (result) => {
+          // Send back the ID of new burger
+          res.json({ id: result.insertId });
+        }
+      );
+    });
+    router.put('/api/burgers/:id', (req, res) => {
+      const condition = 'id = ' + req.params.id;
+  
+      console.log('condition', condition);
+      burger.updateOne({ devoured: req.body.devoured }, condition, (
+        result
+      ) => {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      });
+    });
+    router.delete('/api/burgers/:id', (req, res) => {
+      const condition = 'id = ' + req.params.id;
+      console.log('condition', condition);
+  
+      burger.deleteOne(condition, (result) => {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      });
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
+  
